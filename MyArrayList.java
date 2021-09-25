1e51520827400a31b66d8daa4c1812136340f77e
geojax <50026101+geojax@users.noreply.github.com>
2021-09-25 08:20:17 -0700

    Update MyArrayList.java

--- MyArrayList.java
+++ MyArrayList.java
@@ -13,24 +13,25 @@ public class MyArrayList<E> {
     protected E [] internalArray;
 
     /* Constructor: Create it with whatever capacity you want? */
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public MyArrayList() {
         this.internalArray = (E[])new Object[100];
+        this.startPos = 25;
     }
-    
+
     /* Constructor with initial capacity */
-	@SuppressWarnings("unchecked")
+    @SuppressWarnings("unchecked")
     public MyArrayList(int initialCapacity){
         this.internalArray = (E[])new Object[initialCapacity];
         this.startPos = initialCapacity / 4;
     }
-    
+
     /* Return the number of active slots in the array list */
     @SuppressWarnings("all")
     public int size() {
         return objectCount;
     }
-    
+
     /* Are there zero objects in the array list? */
     public boolean isEmpty() {
         /* ---- YOUR CODE HERE ---- */
@@ -39,13 +40,12 @@ public class MyArrayList<E> {
 
     /* GOOD Get the index-th object in the list. */
     public E get(int index) {
-        /* ---- YOUR CODE HERE ---- */
         if (index >= objectCount || index < 0)
             throw new ArrayIndexOutOfBoundsException("in GET: Index " + index + " out of bounds for length " + size());
 
         return internalArray[index + startPos];
     }
-    
+
     /*GOOD Replace the object at index with obj.  returns object that was replaced. */
     public E set(int index, E obj) {
         /* ---- YOUR CODE HERE ---- */
@@ -58,8 +58,8 @@ public class MyArrayList<E> {
         return replaced;
     }
 
-	/* Returns true if this list contains an element equal to obj;
-	 otherwise returns false. */
+    /* Returns true if this list contains an element equal to obj;
+     otherwise returns false. */
     public boolean contains(E obj) {
         int size = objectCount + startPos - 1;
         // if obj == internalarray[i] or obj.equals(internalarray[i]), then stop
@@ -70,34 +70,40 @@ public class MyArrayList<E> {
         for (; size >= startPos && !(internalArray[size] == obj) && !internalArray[size].equals(obj); size--);
         return size >= startPos;
     }
-    
+
     /* Insert an object at index */
-	//@SuppressWarnings("unchecked")
+    //@SuppressWarnings("unchecked")
     public void add(int index, E obj) {
         if (index > objectCount)
             throw new ArrayIndexOutOfBoundsException();
 
-        if (objectCount == internalArray.length) {
-            increaseCapacity();
+        if (startPos + objectCount == internalArray.length) {
+            increaseCapacityAtEnd();
+        }
+        else if (startPos == 0) {
+            increaseCapacityAtBegin();
         }
 
         // if index at or before middle and space at beginning, shift left from index
-        boolean indexIsBeforeMiddle = index <= objectCount / 2;
-        boolean isSpaceAtBegin = startPos != 0;
-        boolean isSpaceAtEnd = startPos + objectCount < internalArray.length;
-        if ((indexIsBeforeMiddle && isSpaceAtBegin) || !isSpaceAtEnd) {
+//        boolean indexIsBeforeMiddle = ;
+//        boolean isSpaceAtBegin = ;
+//        boolean isSpaceAtEnd = ;
+        // startPos + objectCount == length: startPos + objectCount - 1 is the last index in use,
+        // length - 1 is the last index possible. If these two are equal, then no space at end.
+        int trueIndex = startPos + index;
+        if ((index <= objectCount / 2 /*&& startPos != 0*/) /*|| startPos + objectCount == internalArray.length*/) {
             // shift left from index
-            startPos--;
-            for (int i = startPos; i < index + startPos; i++) {
-                internalArray[i] = internalArray[i + 1];
+            for (int i = startPos; i < trueIndex; i++) {
+                internalArray[i - 1] = internalArray[i];
             }
+            startPos--;
         }
         // otherwise shift right from index
         // note: there IS space at the beginning of the array,
         // or increaseCapacity() would have been called
         else {
             // shift right from index
-            for (int i = startPos + objectCount; i > startPos + index; i--) {
+            for (int i = objectCount; i > trueIndex; i--) {
                 internalArray[i] = internalArray[i - 1];
             }
         }
@@ -136,11 +142,11 @@ public class MyArrayList<E> {
         return ret;
     }
 
-    /* Removes the first occurrence of the specified element from this list, 
-     * if it is present. If the list does not contain the element, it is unchanged. 
+    /* Removes the first occurrence of the specified element from this list,
+     * if it is present. If the list does not contain the element, it is unchanged.
      * More formally, removes the element with the lowest index i such that
-     * (o==null ? get(i)==null : o.equals(get(i))) (if such an element exists). 
-     * Returns true if this list contained the specified element (or equivalently, 
+     * (o==null ? get(i)==null : o.equals(get(i))) (if such an element exists).
+     * Returns true if this list contained the specified element (or equivalently,
      * if this list changed as a result of the call). */
     // MAYBE -- cut loops in half by checking beginning and end, then beginning+1 and end-1, etc.???
     public boolean remove(E obj) {
@@ -154,13 +160,12 @@ public class MyArrayList<E> {
         }
         return false;
     }
-    
+
     /* For testing; your string should output as "[X, X, X, X, ...]" where X, X, X, X, ... are the elements in the ArrayList.
      * If the array is empty, it should return "[]".  If there is one element, "[X]", etc.
      * Elements are separated by a comma and a space. */
     public String toString() {
         StringBuilder retBuilder = new StringBuilder("[");
-        //int size = size();
         int i;
         if (!isEmpty()) {
             retBuilder.append(internalArray[startPos]);
@@ -204,14 +209,26 @@ public class MyArrayList<E> {
         }
     }
 
-    private void increaseCapacity() {
-        E[] grownArr = (E[]) new Object[(internalArray.length == 0) ? 1 : internalArray.length << 1];
-        startPos = grownArr.length / 4;
+    private void increaseCapacityAtEnd() {
+        E[] grownArr = (E[]) new Object[(internalArray.length * 3) / 2 + 1];
+        //startPos = grownArr.length / 4;
         for (int i = 0; i < internalArray.length; i++) {
+            grownArr[i] = internalArray[i];
+        }
+//        System.out.println("Growing to " + grownArr.length);
+//        System.arraycopy(internalArray, 0, grownArr, startPos, objectCount);
+        internalArray = grownArr;
+    }
+
+    private void increaseCapacityAtBegin() {
+        E[] grownArr = (E[]) new Object[(internalArray.length * 3) / 2 + 1];
+        startPos = grownArr.length - internalArray.length;
+        for (int i = internalArray.length - 1; i >= 0; i--) {
             grownArr[i + startPos] = internalArray[i];
         }
-        internalArray = grownArr;
 //        System.out.println("Growing to " + grownArr.length);
+//        System.arraycopy(internalArray, 0, grownArr, startPos, objectCount);
+        internalArray = grownArr;
     }
 
     public int trueSize() {
